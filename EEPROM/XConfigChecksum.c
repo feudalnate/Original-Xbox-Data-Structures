46118bafe5af29bc2b46f36a00dc356bd81c28e4
feudalnate <feudalnate@protonmail.com>
2022-11-06 20:52:17 -0600

    Update XConfigChecksum.c

--- EEPROM/XConfigChecksum.c
+++ EEPROM/XConfigChecksum.c
@@ -1,116 +1,79 @@
 /*
-Iterations of my reversing attempts of the XConfigChecksum function
+ 
+Original XConfigChecksum function (i686/x86 assembly)
 
-The XConfigChecksum function is mainly used for EEPROM sections but is used elsewhere on the system as well
+EAX: Checksum value
+EBX: Number of carries (holds the number of times the carry flag (CF) was set during addition)
+ECX: Pointer to data buffer
+EDX: Number of 32bit values to itterate through (originally passed in as number of bytes in data buffer to process, gets divided by 4)
 
-@NOTE: For EEPROM sections, the returned checksum value of a section MUST have a bitwise NOT applied to the result
+NOTES:
+- Any data not in 32bit alignment is ignored
+- If count is not divisible by 4 then the return value will be 0
+- Bitwise NOT must be applied to the resulting sum if dealing with EEPROM data sections
+- Hard disk "config" sectors use this algorithm and the resulting sum is used as-is
+
+
+mov ecx, [ebp + data]  //store 'data' pointer value, ecx = data pointer
+mov edx, [ebp + count] //store 'count' value, edx = count
+
+xor eax, eax   //zero eax, eax = checksum value
+xor ebx, ebx   //zero ebx, ebx = number of carries
+
+shr edx, 2     //shift count value right by 2 (equivalent of 'count = count / 4')
+test edx, edx  //check count > 0
+jz FinalizeChecksum    //return if count == 0 (must be at least 32bits of data to work on)
+
+Loop:
+add eax, [ecx] //sum = sum + *(unsigned int*)data
+adc ebx, 0     //increment number of carries if CF is set
+add ecx, 4     //increment pointer (data = data + 4)
+dec edx//decrement count (count = count - 1)
+jnz Loop       //continue loop if count > 0
+
+FinalizeChecksum :
+add eax, ebx   //add number of carries to the final checksum value
+adc eax, 0     //add a final carry if (checksum + carries) resulted in CF being set
+
+retn
+
 */
 
-//ORIGINAL KERNEL FUNCTION
-//(neatened up and labeled a bit)
-unsigned int __stdcall XConfigChecksum(void *data, unsigned int count)
-{
-	__asm
-	{
-        push ebp               //store last base pointer for returning to caller
-        mov ebp, esp           //store current stack pointer as our base pointer
-        push ebx               //store return value address
-        mov ecx, [ebp+data]    //store 'data' pointer value
-        mov edx, [ebp+count]   //store count value
-        xor eax, eax           //zero eax 
-        xor ebx, ebx           //zero ebx
-        shr edx, 2             //shift count value right by 2 (equivalent of 'count = count / 4')
-        test edx, edx          //check count > 0
-        jz L2                  //goto L2 if count == 0
-        
-        L1:                    //main loop, += uint32's (+1 on 32bit overflow)
-        add eax, [ecx]         //sum = sum + (unsigned int)*data
-        adc ebx, 0             //+1 (if overflow)
-        add ecx, 4             //increment pointer (*data = *data + 4)
-        dec edx                //decrement count (count = count - 1)
-        jnz L1                 //goto L1 if count > 0
-        
-        L2:
-        add eax, ebx
-        adc eax, 0
-        pop ebx
-        pop ebp
-        retn
-	}
-}
+typedef unsigned char byte;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 
-//FIRST ATTEMPT
-//works but only for C/C++
-unsigned int XConfigChecksum(unsigned char* data, int count)
-{
-#define PACKUINT64(high, low) (((unsigned long long)high) << 32 | low)
-#define CARRYFLAG(x, y) (x > (x + y))
+u32 XConfigChecksum(byte* data, u32 count) {
 
-	unsigned int low = 0;
-	unsigned int high = 0;
-	unsigned long long value = 0;
-	for (int i = count >> 2; i > 0; i--)
-	{
-		value = *(unsigned int*)data + PACKUINT64(high, low);
-		high = (unsigned int)(value >> 32);
-		low = (unsigned int)value;
-		data = (unsigned char*)data + 4;
-	}
-	return (CARRYFLAG(high, low) + (high + low));
-}
+    u64 value = 0;
+    u32 carries = 0;
+    u32 checksum = 0;
 
-//FINAL FUNCTION
-//this works in C/C++ and C# (with a couple tweaks)
-unsigned long XConfigChecksum(unsigned char* data, int count)
-{
-    unsigned long result = 0;
-    unsigned long overflows = 0;
-    unsigned long long value;
-    for (int i = 0; i < (count / 4); i++)
-    {
-        value = ((unsigned long long)result + *(unsigned long*)(data + (i * 4)));
-        if (value > 0xFFFFFFFF) overflows++;
-        result = (unsigned long)value;
+    const u64 lower_mask = 0xFFFFFFFF00000000;
+    const u64 upper_mask = 0x00000000FFFFFFFF;
+
+    if (data && count > 0) {
+
+        for (u32 i = 0; i < (count / 4); i++) {
+
+            value = checksum;
+            value += *(u32*)(data + (i * 4));
+
+            if ((value & lower_mask) > 0)
+                carries++;
+
+            checksum = (value & upper_mask);
+        }
+
+        if (carries > 0) {
+            value = checksum;
+            value += carries;
+            checksum = (value & upper_mask);
+
+            if ((value & lower_mask) > 0)
+                checksum++;
+        }
     }
-    value = ((unsigned long long)result + overflows);
-    overflows = 0;
-    if (value > 0xFFFFFFFF) overflows++;
-    result = (unsigned long)value;
-    return (result + overflows);
-}
 
-//FINAL FINAL function
-//no casting, tried to keep within bounds of 8086 registers
-//easily portable to other languages
-unsigned int XConfigChecksum(unsigned char* data, unsigned int count)
-{
-    unsigned char* ecx; //data
-    unsigned int edx;   //count
-    unsigned int eax;   //checksum
-    unsigned int ebx;   //num. of carries (overflows)
-
-    ecx = data;
-    edx = (count >> 2);
-    eax = 0;
-    ebx = 0;
-
-    if (edx == 0)
-        goto L2;
-
-L1:
-    if (eax > (*(unsigned int*)ecx + eax))
-        ebx++;
-    eax = (*(unsigned int*)ecx + eax);
-    ecx += 4;
-    edx--;
-    if (edx > 0)
-        goto L1;
-
-L2:
-    if (eax > (eax + ebx))
-        eax = (eax + ebx) + 1;
-    else
-        eax = (eax + ebx);
-
-    return eax;
+    return checksum;
 }
